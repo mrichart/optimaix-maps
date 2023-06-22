44e8cb4d538fc87f7e66b497774297cfa2f1281e
Matias Richart <mrichart@fing.edu.uy>
2023-06-22 12:14:06 +0200

    Delete old files

--- static-allocation/delay_constraints/optimal/matlab/createB.m
+++
@@ -1,7 +0,0 @@
-function b = createB(sigma, beta, kappa, cant_services, cant_microservices)
-%createB Create vector of constraints
-%   receives vector of storage, bandwicth and computing capacity of each
-%   server
-
-b = [sigma'; beta'; kappa'; ones(cant_services*cant_microservices,1)];
-end
